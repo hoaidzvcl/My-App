5f60451630ff02931b04537dcf13b97e8bf71ea4
hoaidzvcl <hoaixoai268@gmail.com>
2024-09-24 17:03:43 +0700

    second

---
+++ src/routes/quan-li-nguoi-dung/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {onMount} from "svelte";
+    
+    let users = [];
+
+    onMount(async () => {
+        const res = await fetch(`http://10.0.2.14:3000/user`);
+        users = (await res.json()).data;
+    })
+</script>
+
+<a href="quan-li-nguoi-dung/them-nguoi-dung">Them moi</a>
+
+<table>
+    <thead>
+        <th>STT</th>
+        <th>Ho va Ten</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Trang thai</th>
+    </thead>
+
+    <tbody>
+        {#each users as user, i(i)}
+            <tr>
+                <td>{i+1}</td>
+                <td>{user.full_name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.active}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
---
+++ src/routes/quan-li-nguoi-dung/them-nguoi-dung/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {goto} from "$app/navigation"
+    let user = {};
+
+    async function luu(){
+        let res = await fetch("http://10.0.2.14:3000/user",{
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        if (res.ok) {
+            goto("/quan-li-nguoi-dung");
+        } else {
+            alert("Them moi that bai")
+        }
+    };
+</script>
+
+{JSON.stringify(user)};
+
+<form action="" on:submit={luu}>
+    <label for="full-name">Ho va ten</label>
+    <input id="full-name" type="text" bind:value={user.full_name}>
+    <label for="username">Username</label>
+    <input id="username" type="text" bind:value={user.username}>
+    <label for="email">Email</label>
+    <input id="email" type="email" bind:value={user.email}>
+    <label for="mat-khau">Password</label>
+    <input id="mat-khau" type="password" bind:value={user.password}>
+
+    <button type="submit">Luu</button>
+</form>
